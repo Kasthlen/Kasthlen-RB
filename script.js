891ca112322637327dae0f60fb377e5c493f9e95
Kasthlen <kasthlen97@gmail.com>
2024-11-15 13:56:37 +0100

    script

---
+++ script.js
@@ -0,0 +1,36 @@
+/*alert("Bienvenidoooooo"); muestra una ventana emergente
+alert(5*3+2)
+*/
+
+let saludo = "helllooooooo";
+
+let despedida;
+despedida = "bye byeeeee";
+
+saludo = "que mas pueeees";
+
+let nombreUsuario = " kasthlen";
+let edad = 16;
+
+/*alert(saludo+""+nombreUsuario); //puedo concatenar strings usando +  (camellCase) cada inicial en mayuscula
+
+alert(despedida);
+
+
+edad = 17; // cambio mi valor
+edad+1; // suma 1
+edad++; // suma 1
+edad--; // resta 1
+// estos sirven para sumar mi numero*/
+
+console.log(edad);
+// muestra lo que pongamos entre parentesis en mi consola
+
+//LAS CONSTANTES LAS PONGO  EN MAYUSCULAS PARA DIFERENCIAR MIS VARIABLES
+const SALUDO_USUARIO = saludo+""+nombreUsuario;
+/*saludo += nombreUsuario; //le suma el valor
+saludo = nombreUsuario //cambia el valor */
+
+alert(SALUDO_USUARIO)
+
+ 
